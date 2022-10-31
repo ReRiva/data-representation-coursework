857e08455ef51d89be532bcf96eeee2b926b8cbd
Renan Riva <renanriva@hotmail.com>
2022-10-31 11:33:49 +0000

    Assignment 4

---
+++ Assignments/config.py
@@ -0,0 +1,3 @@
+
+config = {"html2pdf":"fpSA7ZpPasRGNeNDHZjZEZ2rNSf1adXEkE19vIAficRXYYDUB4ykn578p31awzbB"}
+github = {"github": "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"}
